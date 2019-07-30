099f547a8ae0807ae085a59773a433c4d05946ff
Xiangdong Gu <xgu@massmutual.com>
2019-07-30 15:41:06 +0800

    public housing transaction

--- R/hk-housing.R
+++ R/hk-housing.R
@@ -41,7 +41,7 @@ public_housing_rental <- function() {
   do.call("bind_rows", data)
 }
 
-#' Public housing estates (Annually update)
+#' Location and Profile of Public housing estates (Annually update)
 #' 
 #' @param lang the language: en, tc, sc
 #' 
@@ -53,5 +53,24 @@ public_housing_estates <- function(lang = 'en') {
   url <- sprintf(url, lang)
   res <- GET(url)
   jsonlite::fromJSON(content(res, "text"))$data
+}
+
+#' Transactions in the Sale of Home Ownership Scheme (HOS) Flats and 
+#' Transactions in the Sale of Green Form Subsidised Home Ownership Scheme (GSH) 
+#' Flats
+#' 
+#' @param year the year of data currently only 2018, will adjust when more data
+#' available
+#' 
+#' @export
+#' 
+public_housing_trans <- function(year = 2018) {
+  url1 <- paste0("https://data.housingauthority.gov.hk/psi/rest/export/ssfs-sale-",
+                "transactions-018/?lang=en&format=json")
+  url2 <- paste0("https://data.housingauthority.gov.hk/psi/rest/export/ssfs-sale-",
+                 "transactions-G18/?lang=en&format=json")
+  d1 <- jsonlite::fromJSON(content(GET(url1), "text"))$data
+  d2 <- jsonlite::fromJSON(content(GET(url2), "text"))$data
+  bind_rows(d1, d2)
 }
 
