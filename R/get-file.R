6823a17d951ea66a230163687945ae34073f5fdf
Cecilia Lee <ceciliahslee@gmail.com>
2019-08-01 16:26:01 +0800

    Default get file download message to be silent.

--- R/get-file.R
+++ R/get-file.R
@@ -10,7 +10,7 @@
 #'
 #' @export
 #'
-get_file_xlsx <- function(url, path = ".", silent = FALSE) {
+get_file_xlsx <- function(url, path = ".", silent = TRUE) {
   path <- file.path(path, basename(url))
   download.file(url, path, quiet = TRUE)
   if (!silent) cat(sprintf("Downloaded to %s\n", path))
@@ -23,6 +23,7 @@ get_file_xlsx <- function(url, path = ".", silent = FALSE) {
 #' @param url the url of the file
 #' @param path the directory where the file should be save, if NULL it
 #' will not save
+#' @param silient whether or not silent message
 #'
 #' @return it returns a data frame read from the csv file
 #'
@@ -31,7 +32,7 @@ get_file_xlsx <- function(url, path = ".", silent = FALSE) {
 #'
 #' @export
 #'
-get_file_csv <- function(url, path = NULL, col_names = TRUE, col_types = NULL) {
+get_file_csv <- function(url, path = NULL, col_names = TRUE, col_types = NULL, silent = TRUE) {
   require(readr)
   if (is.null(path)) {
     path <- file.path(tempdir(), basename(url))
@@ -40,7 +41,7 @@ get_file_csv <- function(url, path = NULL, col_names = TRUE, col_types = NULL) {
     path <- file.path(path, basename(url))
   }
   download.file(url, path, quiet = TRUE)
-  cat(sprintf("Downloaded to %s\n", path))
+  if (!silent) cat(sprintf("Downloaded to %s\n", path))
   data <- read_csv(path, col_name = col_names, col_types = col_types)
   data
 }
@@ -50,16 +51,17 @@ get_file_csv <- function(url, path = NULL, col_names = TRUE, col_types = NULL) {
 #' @param url the url of the file
 #' @param path the directory to save the file
 #' @param view if TRUE the the file is viewed after download
+#' @param silient whether or not silent message
 #'
 #' @examples
 #' get_file_jpg("http://www.toothclub.gov.hk/chi/jpg/psi/P041.jpg")
 #'
 #' @export
 #'
-get_file_jpg <- function(url, path = ".", view = TRUE) {
+get_file_jpg <- function(url, path = ".", view = TRUE, silent = TRUE) {
   path <- file.path(path, basename(url))
   download.file(url, path, quiet = TRUE)
-  cat(sprintf("Downloaded to %s\n", path))
+  if (!silent) cat(sprintf("Downloaded to %s\n", path))
   if (view) {
     viewer <- getOption("viewer")
     viewer(path)
@@ -71,6 +73,7 @@ get_file_jpg <- function(url, path = ".", view = TRUE) {
 #' @param url the url of the file
 #' @param path the directory where the file should be save, if NULL it
 #' will not save
+#' @param silient whether or not silent message
 #'
 #' @return it returns a data read from the json file
 #'
@@ -79,7 +82,7 @@ get_file_jpg <- function(url, path = ".", view = TRUE) {
 #'
 #' @export
 #'
-get_file_json <- function(url, path = NULL) {
+get_file_json <- function(url, path = NULL, silent = TRUE) {
   if (is.null(path)) {
     path <- file.path(tempdir(), basename(url))
     on.exit(unlink(path))
@@ -87,7 +90,7 @@ get_file_json <- function(url, path = NULL) {
     path <- file.path(path, basename(url))
   }
   download.file(url, path, quiet = TRUE)
-  cat(sprintf("Downloaded to %s\n", path))
+  if (!silent) cat(sprintf("Downloaded to %s\n", path))
   data <- jsonlite::fromJSON(path)
   data
 }
