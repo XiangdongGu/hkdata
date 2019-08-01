c7aa03c3e894c2240408a45bdefd09166e165130
AlfredSAM <asam46@massmutual.com>
2019-08-01 09:25:54 +0800

    Update functions to transport

--- R/hk-transport.R
+++ R/hk-transport.R
@@ -1,5 +1,8 @@
 
 
+source("get-file.R")
+source("list-file.R")
+
 
 # (1) Details of HK Electric EV charging stations
 # https://data.gov.hk/en-data/dataset/hkelectric-tnd_cs_ci-hkelectric-ev-location/resource/d4aa7487-1eb7-4b95-b552-d0c7b0029415
@@ -7,31 +10,31 @@
 # Given the lat_in and long_in of the place, the function can return the information of the charging stations around the place and
 # arranged by the distances
 
-EV_charging_stations <- function(lat_in,long_in) {
+transport_EV_charging <- function(lat_in,long_in) {
   require(dplyr)
-
+  
   url <- paste("https://api.data.gov.hk/v1/nearest-hke-electric-vehicle-charging-stations?lat=",lat_in,sep="")
   url <- paste(url,"&long=",sep="")
   url <- paste(url,long_in,sep="")
   temp_file <- get_file_json(url)
   temp_file <- temp_file$results
-
+  
   temp_file <- temp_file %>%
     mutate(lat = unlist(lapply(1:nrow(temp_file), function(x){temp_file$"lat-long"[x][[1]][1]})),
            long = unlist(lapply(1:nrow(temp_file), function(x){temp_file$"lat-long"[x][[1]][2]})),
            distance = sqrt((lat-lat_in)^2+(long-long_in)^2))  %>%
     arrange(distance)
-
-
+  
+  
   return(temp_file)
-
+  
 }
 
 
 
 
 # #Example:
-# test1<-EV_charging_stations(22,114)
+# test1<-transport_EV_charging(22,114)
 
 
 
@@ -53,18 +56,18 @@ EV_charging_stations <- function(lat_in,long_in) {
 
 
 
-Arrival_CrossBoundaryFerry <- function(timestamp){
+transport_Arrival_CrossBoundaryFerry <- function(timestamp){
   #timestamp format: "20190729-0915"
   url <- "https://www.mardep.gov.hk/e_files/en/opendata/arrival_en.csv"
   url <- hist_file_url(url,timestamp)
   temp_file <-read.csv(url, sep = "|")
-
+  
 }
 
 
 
 # #Example
-# test1 <- Arrival_CrossBoundaryFerry("20190729-1004")
+# test1 <- transport_Arrival_CrossBoundaryFerry("20190729-1004")
 
 
 
@@ -82,115 +85,172 @@ Arrival_CrossBoundaryFerry <- function(timestamp){
 # https://www.hongkongairport.com/flightinfo-rest/rest/flights/past?date=%3Cdate%3E&lang=en&cargo=false&arrival=false
 
 
-Arrival_Flight <- function(date){
-  #date format:  "YYYY-MM-DD"
-  require(tidyr)
-  url = paste("https://www.hongkongairport.com/flightinfo-rest/rest/flights/past?date=",date,sep="")
-  url = paste(url,"&lang=en&cargo=false&arrival=false",sep="")
+
+transport_Flight <- function(Date, Arrival, Cargo, Lang = "en") {
+  
+  # Parameter                   Description                                          Type
+  # Date Date of the records.  The data is up-to-date to theprevious calendar day.   String, “YYYY-MM-DD”
+  # Arrival                    Arrival or departure                                  Bool (TRUE=arrival, FALSE=departure)
+  # Cargo                      Cargo or passenger flight                             Bool (TRUE=cargo, FALSE=passenger)
+  # Lang                       Language                                              en, zh_HK or zh_CN
+  
+  require(tidyverse)
+  require(data.table)
+  
+  url = paste("https://www.hongkongairport.com/flightinfo-rest/rest/flights/past?date=",Date,sep="")
+  url = paste(url,"&lang=",sep = "")
+  url = paste(url,Lang,sep = "")
+  url = paste(url, "&cargo=",sep = "")
+  url = paste(url, tolower(as.character(Cargo)), sep = "")
+  url = paste(url, "&arrival=", sep = "")
+  url = paste(url,tolower(as.character(Arrival)), sep = "")
   temp_file = get_file_json(url)
-  temp_file = crossing(temp_file["date","arrival","cargo"],temp_file[[4]][[1]])
-
-}
-
-test1 <- Arrival_Flight("2019-05-01")
-
-
-
-
-require(RJSONIO)  
-
-
-temp_file = RJSONIO::fromJSON(url) #get the list
-
-
-
-
-
-
-
-
-test1.1 <- as.data.frame(do.call("cbind", temp_file))
-
-
-test2 <- jsonlite::fromJSON(url) #get the data frame
-
-
-
-test2
-
-
-lapply(1:nrow(test2), function(x){ 
-  lapply(     )
   
-  test2[[4]][[x]]}
-
+  
+  temp1 <- temp_file %>%
+    mutate_if(is.list, map, as_data_frame) %>%
+    unnest() %>%
+    select (-flight)
   
   
   
-  
-  test3 <- (test2[[4]][[2]])[[6]][[1]]
-  
-  
-  
-  
-  
-  
-  (test2[[4]][[2]])[[6]][[1]] %>%
-    mutate(destination = (test2[[4]][[2]])[[1]][[1]],
-            terminal = (test2[[4]][[2]])[[2]][[1]],
-           aisle = (test2[[4]][[2]])[[3]][[1]],
-           gate = (test2[[4]][[2]])[[4]][[1]], 
-           time = (test2[[4]][[2]])[[5]][[1]],
-           status = (test2[[4]][[2]])[[7]][[1]],
-           statusCode = (test2[[4]][[2]])[[8]][[1]])
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  lapply(c(1:10),
-         
-         function(x){
-           (test2[[4]][[2]])[[6]][[x]] %>%
-             mutate(destination = (test2[[4]][[2]])[[1]][[x]],
-                    terminal = (test2[[4]][[2]])[[2]][[x]],
-                    aisle = (test2[[4]][[2]])[[3]][[x]],
-                    gate = (test2[[4]][[2]])[[4]][[x]], 
-                    time = (test2[[4]][[2]])[[5]][[x]],
-                    status = (test2[[4]][[2]])[[7]][[x]],
-                    statusCode = (test2[[4]][[2]])[[8]][[x]]
-             )
-           
-           
-         }
-         
-         )
-  
-  
-  
-  (test2[[4]][[2]])[[6]][[1]] %>%
-    mutate(terminal = (test2[[4]][[2]])[[2]][[1]],
-           aisle = (test2[[4]][[2]])[[3]][[1]],
-           gate = (test2[[4]][[2]])[[4]][[1]], 
-           time = (test2[[4]][[2]])[[5]][[1]],
-           status = (test2[[4]][[2]])[[7]][[1]],
-           statusCode = (test2[[4]][[2]])[[8]][[1]])
-  for i in 1:length((test2[[4]][[2]])[[1]][[1]]) {
+  if (Cargo){
+    
+    if (Arrival) {
+      temp2 <- temp_file %>%
+        mutate_if(is.list, map, as_data_frame) %>%
+        unnest() %>%
+        select (-origin)
+    } else {
+      temp2 <- temp_file %>%
+        mutate_if(is.list, map, as_data_frame) %>%
+        unnest() %>%
+        select (-destination)
+    }
+    
+    
+    
+    temp3 <- lapply(1:nrow(temp_file[[4]][[1]]), function(x){
+      full_join(unnest(temp1[x,]),unnest(temp2[x,]),
+                by = c("date", "arrival", "cargo","time", "statusCode", "status"))
+    })
+    
     
   }
-  destination = (test2[[4]][[2]])[[1]][[1]],
+  
+  
+  else{
+    
+    if (Arrival) {
+      temp2 <- temp_file %>%
+        mutate_if(is.list, map, as_data_frame) %>%
+        unnest() %>%
+        select (-origin)
+    } else {
+      temp2 <- temp_file %>%
+        mutate_if(is.list, map, as_data_frame) %>%
+        unnest() %>%
+        select (-destination)
+    }
+    
+    
+    if (Arrival) {
+      temp3 <- lapply(1:nrow(temp_file[[4]][[1]]), function(x){
+        full_join(unnest(temp1[x,]),unnest(temp2[x,]),
+                  by = c("date", "arrival", "cargo", "baggage", "hall", "terminal", "stand", "time","statusCode", "status" ))
+      })
+    } else {
+      temp3 <- lapply(1:nrow(temp_file[[4]][[1]]), function(x){
+        full_join(unnest(temp1[x,]),unnest(temp2[x,]),
+                  by = c("date", "arrival", "cargo", "terminal", "aisle", "gate", "time", "statusCode", "status"))
+      })
+    }
+    
+    
+  }
   
   
   
+  temp3 <- rbindlist(temp3)
+  
+  return(temp3)
+  
+}
+
+
+
+# #Example
+# 
+# test1<-transport_Flight("2019-05-01",FALSE,FALSE)
+# 
+# test2<-transport_Flight("2019-05-01",TRUE,FALSE)
+# 
+# test3<-transport_Flight("2019-05-01",FALSE,TRUE)
+# 
+# test4<-transport_Flight("2019-05-01",TRUE,TRUE)
+
+
+
+
+
+
+
+# (4)Parking vacancy data
+# https://data.gov.hk/en-data/dataset/hk-td-tis_5-real-time-parking-vacancy-data
+# 
+# Instructions: http://static.data.gov.hk/td/parking-vacancy/dataspec/TD_Parking_Vacancy_Data_Specification.pdf
+
+
+
+# Two sub_tables:
+#   
+# (4.1)  Basic Information of Participating Car Parks
+# https://data.gov.hk/en-data/dataset/hk-td-tis_5-real-time-parking-vacancy-data/resource/01f937a6-323d-4347-bd2e-e366a8ae3375
+# UPDATE FREQUENCY: FROM HOURLY TO REAL-TIME
+
+transport_carpark_basic <- function(timestamp){
+  #timestamp format: "20190705-0015"
+  url <- "http://resource.data.one.gov.hk/td/carpark/basic_info_all.json"
+  url <- hist_file_url(url,timestamp)
+  temp_file = get_file_json(url)
+  
+  temp_file <- temp_file[[1]]
+  
+}
+
+
+# #Example  
+# test1<-transport_carpark_basic("20190705-0015")
+
+
+
+
+
+# (4.2) Parking Vacancy Data of Participating Car Parks
+# https://data.gov.hk/en-data/dataset/hk-td-tis_5-real-time-parking-vacancy-data/resource/ab3c69cf-72fa-4f0a-92c1-cf97a08d3a79
+# UPDATE FREQUENCY: FROM HOURLY TO REAL-TIME
+
+
+transport_carpark_vacancy <- function(timestamp){
+  require(data.table)
+  #timestamp format: "20190701-0030"
+  url <- "http://resource.data.one.gov.hk/td/carpark/vacancy_all.json"
+  url <- hist_file_url(url,timestamp)
+  temp_file = get_file_json(url)
+  temp_file <-  unnest(temp_file[[1]])
+  temp_file  <- lapply(1:nrow(temp_file), function(x){
+    unnest(temp_file[x,])
+  })
+  temp_file <- rbindlist(temp_file)
+  
+  return(temp_file)
   
   
-  
-  
-  
-  
-  
+}
+
+# #Example
+# test1 <- transport_carpark_vacancy("20190701-0030")
+
+
+
+
