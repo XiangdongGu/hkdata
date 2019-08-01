e08f25e6e73470466b43a8404b9415c651c75a9b
AlfredSAM <asam46@massmutual.com>
2019-08-01 16:58:01 +0800

    Update functions to transport

--- R/hk-transport.R
+++ R/hk-transport.R
@@ -104,9 +104,15 @@ transport_Arrival_CrossBoundaryFerry <- function(timestamp = NULL, Lang = "en"){
 
 
 
-# #Example
+# #Example for retrieving data for specific timestamp
 # test1 <- transport_Arrival_CrossBoundaryFerry("20190729-1004")
-
+# 
+# #Example for retrieving all historical versions of data for specified time range
+# #One can go to website https://data.gov.hk/en-data/dataset/hk-md-mardep-crossboundaryferryservices-arrive/resource/c0b4b251-99c5-472d-adc9-bdd1dfd6c6b8
+# #to check the versions of data within some range.
+# #This example can retrieve  all versions of data within specified range
+# test2 <- get_file_versions(url = transport_Arrival_CrossBoundaryFerry_url(),
+#                            start = "2019-07-31", retrieve_func = transport_Arrival_CrossBoundaryFerry_retrieve)
  
 
 
@@ -118,19 +124,25 @@ transport_Arrival_CrossBoundaryFerry <- function(timestamp = NULL, Lang = "en"){
 # Instruction: https://www.hongkongairport.com/iwov-resources/misc/opendata/Flight_Information_DataSpec_en.pdf
 # UPDATE FREQUENCY: DAILY (UPDATED TO PREVIOUS CALENDAR DAY)
 
-
-
 # https://www.hongkongairport.com/flightinfo-rest/rest/flights/past?date=%3Cdate%3E&lang=en&cargo=false&arrival=false
 
 
 
-transport_Flight <- function(Date, Arrival, Cargo, Lang = "en") {
+
+#' Generate the Flight Information given the date, arrival or departure, cargo or pessenger for different language versions
+#'
+#' @param Date String, "YYYY-MM-DD" (The data is up-to-date to the previous calendar day.)
+#'
+#' @param Arrival Arrival or departure (TRUE=arrival, FALSE=departure)
+#'
+#' @param Cargo Cargo or passenger flight (TRUE=cargo, FALSE=passenger)
+#' 
+#' @param Lang Options of Language: "zh_HK": traditional Chinese; "zh_CN": simplified Chinese; "en": English (default)
+#'
+#' @export
+#'
+transport_Flight_retrieve <- function(Date, Arrival, Cargo, Lang = "en") {
   
-  # Parameter                   Description                                          Type
-  # Date Date of the records.  The data is up-to-date to theprevious calendar day.   String, “YYYY-MM-DD”
-  # Arrival                    Arrival or departure                                  Bool (TRUE=arrival, FALSE=departure)
-  # Cargo                      Cargo or passenger flight                             Bool (TRUE=cargo, FALSE=passenger)
-  # Lang                       Language                                              en, zh_HK or zh_CN
   
   require(tidyverse)
   require(data.table)
@@ -216,19 +228,39 @@ transport_Flight <- function(Date, Arrival, Cargo, Lang = "en") {
 }
 
 
+#' Generate the list of Flight Information given the time range, arrival or departure, cargo or pessenger for different language versions
+#'
+#' @param start String, "YYYY-MM-DD"  starting date of interest
+#' 
+#' @param end String, "YYYY-MM-DD"  ending date of interest
+#'
+#' @param Arrival Arrival or departure (TRUE=arrival, FALSE=departure)
+#'
+#' @param Cargo Cargo or passenger flight (TRUE=cargo, FALSE=passenger)
+#' 
+#' @param Lang Options of Language: "zh_HK": traditional Chinese; "zh_CN": simplified Chinese; "en": English (default)
+#'
+#' @export
+#'
+transport_Flight <- function(start, end, Arrival, Cargo, Lang = "en") {
+  lapply(seq(as.Date(start), as.Date(end),"days"), function(x){transport_Flight_retrieve(x,Arrival, Cargo, Lang)}  )
+}
 
-# #Example
-# 
-# test1<-transport_Flight("2019-07-01",FALSE,FALSE)
-# 
-# test2<-transport_Flight("2019-07-01",TRUE,FALSE)
-# 
-# test3<-transport_Flight("2019-07-01",FALSE,TRUE)
-# 
-# test4<-transport_Flight("2019-07-01",TRUE,TRUE)
 
 
-
+# #Example for retrieving flight information data for sepecified day
+# 
+# test1<-transport_Flight_retrieve("2019-07-01",FALSE,FALSE)
+# 
+# test2<-transport_Flight_retrieve("2019-07-01",TRUE,FALSE,"zh_HK")
+# 
+# test3<-transport_Flight_retrieve("2019-07-01",FALSE,TRUE,"zh_CN")
+# 
+# test4<-transport_Flight_retrieve("2019-07-01",TRUE,TRUE)
+# 
+# #Example for retrieving flight information data for the time range. It returns the list of data frame for each date within the range. 
+# test5 <- transport_Flight("2019-07-29","2019-07-31",FALSE,FALSE)
+  
 
 
 
@@ -246,20 +278,55 @@ transport_Flight <- function(Date, Arrival, Cargo, Lang = "en") {
 # https://data.gov.hk/en-data/dataset/hk-td-tis_5-real-time-parking-vacancy-data/resource/01f937a6-323d-4347-bd2e-e366a8ae3375
 # UPDATE FREQUENCY: FROM HOURLY TO REAL-TIME
 
-transport_carpark_basic <- function(timestamp){
-  #timestamp format: "20190705-0015"
-  url <- "http://resource.data.one.gov.hk/td/carpark/basic_info_all.json"
-  url <- hist_file_url(url,timestamp)
-  temp_file = get_file_json(url)
-  
-  temp_file <- temp_file[[1]]
-  
+
+#' Generate the basic url for Basic Information of Participating Car Parks
+#'
+#'
+#' @export
+#'
+transport_carpark_basic_url <- function(){
+  return("http://resource.data.one.gov.hk/td/carpark/basic_info_all.json")
 }
 
 
-# #Example  
-# test1<-transport_carpark_basic("20190705-0015")
+#' Retrieve the Basic Information of Participating Car Parks given specific data url
+#'
+#' @param data_url Specific data url
+#'
+#' @export
+#'
+transport_carpark_basic_retrieve <- function(data_url){
+  temp_file = get_file_json(data_url)
+  temp_file = temp_file[[1]]
+  return(temp_file)
+}
 
+
+
+#' Retrieve the Basic Information of Participating Car Parks given specific timestamp 
+#'
+#' @param timestamp Timestamp of the historical file to retrieve. If null then
+#'   current, otherwise historical. It should be in format of \%Y\%m\%d-\%H\%M,
+#'   e.g. 20180905-1306.
+#' @export
+#'
+transport_carpark_basic <- function(timestamp = NULL){
+  data_url <- data_file_url(transport_carpark_basic_url(), timestamp)
+  temp_tile <- transport_carpark_basic_retrieve(data_url)
+  return(temp_tile)
+}
+
+
+
+# #Example to retrieve data of asic Information of Participating Car Parks given timestamp
+# test1<-transport_carpark_basic("20190705-0015")
+# 
+# #Example for retrieving all historical versions of data for specified time range
+# #One can go to website https://data.gov.hk/en-data/dataset/hk-td-tis_5-real-time-parking-vacancy-data/resource/01f937a6-323d-4347-bd2e-e366a8ae3375
+# #to check the versions of data within some range.
+# #This example can retrieve all versions of data within specified range
+# test2 <- get_file_versions(url = transport_carpark_basic_url(),
+#                            start = "2019-07-31", retrieve_func = transport_carpark_basic_retrieve)
 
 
 
@@ -269,26 +336,62 @@ transport_carpark_basic <- function(timestamp){
 # UPDATE FREQUENCY: FROM HOURLY TO REAL-TIME
 
 
-transport_carpark_vacancy <- function(timestamp){
+
+#' Generate the basic url for Parking Vacancy Data of Participating Car Parks
+#'
+#' @export
+#'
+transport_carpark_vacancy_url <- function(){
+  return("http://resource.data.one.gov.hk/td/carpark/vacancy_all.json")
+}
+
+
+#' Retrieve the Parking Vacancy Data of Participating Car Parks given specific data url
+#'
+#' @param data_url Specific data url
+#'
+#' @export
+#'
+transport_carpark_vacancy_retrieve <- function(data_url){
   require(data.table)
-  #timestamp format: "20190701-0030"
-  url <- "http://resource.data.one.gov.hk/td/carpark/vacancy_all.json"
-  url <- hist_file_url(url,timestamp)
-  temp_file = get_file_json(url)
+  
+  temp_file = get_file_json(data_url)
   temp_file <-  unnest(temp_file[[1]])
   temp_file  <- lapply(1:nrow(temp_file), function(x){
     unnest(temp_file[x,])
   })
   temp_file <- rbindlist(temp_file)
-  
   return(temp_file)
-  
   
 }
 
+
+
+#' Retrieve the Parking Vacancy Data of Participating Car Parks given specific timestamp 
+#'
+#' @param timestamp Timestamp of the historical file to retrieve. If null then
+#'   current, otherwise historical. It should be in format of \%Y\%m\%d-\%H\%M,
+#'   e.g. 20180905-1306.
+#' @export
+#'
+transport_carpark_vacancy <- function(timestamp = NULL){
+  data_url <- data_file_url(transport_carpark_vacancy_url(), timestamp)
+  temp_tile <- transport_carpark_vacancy_retrieve(data_url)
+  return(temp_tile)
+}
+
+
+
+
 # #Example
 # test1 <- transport_carpark_vacancy("20190701-0030")
-
+# 
+# #Example for retrieving all historical versions of data for specified time range
+# #One can go to website https://data.gov.hk/en-data/dataset/hk-td-tis_5-real-time-parking-vacancy-data/resource/ab3c69cf-72fa-4f0a-92c1-cf97a08d3a79
+# #to check the versions of data within some range.
+# #This example can retrieve all versions of data within specified range
+# test2 <- get_file_versions(url = transport_carpark_vacancy_url(),
+#                            start = "2019-07-31", retrieve_func = transport_carpark_vacancy_retrieve)
 
 
 
